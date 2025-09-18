f46ad76adf55d64e650e383503a0cdc3ae8a03ba
Rytis Grincevicius <rytis.grincevicius@gmail.com>
2025-09-18 14:06:06 +0300

    chore: initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,74 @@
+import nx from '@nx/eslint-plugin';
+
+export default [
+  ...nx.configs['flat/base'],
+  ...nx.configs['flat/typescript'],
+  ...nx.configs['flat/javascript'],
+  {
+    ignores: [
+      '**/dist',
+      '**/vite.config.*.timestamp*',
+      '**/vitest.config.*.timestamp*',
+      '**/test-output',
+    ],
+  },
+  {
+    files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
+    rules: {
+      '@nx/enforce-module-boundaries': [
+        'error',
+        {
+          enforceBuildableLibDependency: true,
+          allow: ['^.*/eslint(\\.base)?\\.config\\.[cm]?[jt]s$'],
+          depConstraints: [
+            {
+              sourceTag: '*',
+              onlyDependOnLibsWithTags: ['*'],
+            },
+          ],
+        },
+      ],
+    },
+  },
+  {
+    files: [
+      '**/*.ts',
+      '**/*.tsx',
+      '**/*.cts',
+      '**/*.mts',
+      '**/*.js',
+      '**/*.jsx',
+      '**/*.cjs',
+      '**/*.mjs',
+    ],
+    // Override or add rules here
+    rules: {
+      /** Bans the use of inline type-only markers for named imports */
+      'import/consistent-type-specifier-style': ['error', 'prefer-top-level'],
+      /** Reports any imports that come after non-import statements */
+      'import/first': 'error',
+      /** Stylistic preference */
+      'import/newline-after-import': 'error',
+      /** No require() or module.exports */
+      'import/no-commonjs': 'error',
+      /** Reports if a resolved path is imported more than once */
+      'import/no-duplicates': 'error',
+      /** Stylistic preference */
+      'import/order': [
+        'error',
+        {
+          groups: [
+            'builtin',
+            'external',
+            'internal',
+            'parent',
+            'sibling',
+            'index',
+            'object',
+            'type',
+          ],
+        },
+      ],
+    },
+  },
+];
