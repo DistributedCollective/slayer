a0cb862f8d67e67ca7a550d8d425f491bc47695d
Rytis Grincevicius <rytis.grincevicius@gmail.com>
2025-09-23 09:30:01 +0300

    fix: github workflows

--- apps/indexer/src/env.ts
+++ apps/indexer/src/env.ts
@@ -1,13 +1,15 @@
 import 'dotenv/config';
-import { bool, cleanEnv, port, str } from 'envalid';
+import { bool, cleanEnv, port, str, testOnly } from 'envalid';
 
 export const ENV = cleanEnv(process.env, {
   PORT: port({ default: 8000 }),
   HOST: str({ default: '0.0.0.0' }),
   NODE_ENV: str({ default: 'development' }),
 
-  DATABASE_URL: str(),
-  REDIS_URL: str(),
+  DATABASE_URL: str({
+    default: testOnly('postgres://postgres:secret@localhost:5432/db'),
+  }),
+  REDIS_URL: str({ default: testOnly('redis://localhost:6379') }),
 
   CORS_ORIGINS: str({ default: '*' }),
 
